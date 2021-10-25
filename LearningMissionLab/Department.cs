795b8c0989c1537b41e49d67d3a2bbc1a18ee6e3
Vahe Mkrtchyan <martuni3vahe@gmail.com>
2021-10-25 21:29:27 +0400

    craeted Department class

---
+++ LearningMissionLab/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningMissionLab
+{
+    public class Department : Unit<Employee>
+    {
+        public int companyId;
+        public string contactInfo;
+
+
+        public Department()
+        {
+
+        }
+
+        public int CompanyId { get => companyId; set => companyId = value; }
+        public string ContactInfo { get => contactInfo; set => contactInfo = value; }
+
+    }
+}
