04ab818b94fe89b3d73857bc90290f3b53f138ea
edvard1998 <e.soghomonyan98@gmail.com>
2021-10-25 20:53:24 +0400

    created unit class, added constructors and properties

---
+++ LearningMissionLab/UnitType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningMissionLab
+{
+    public enum UnitType
+    {
+        Unspecified,
+        Company,
+        Department,
+        Classroom,
+        Subject,
+        Module
+    }
+}
