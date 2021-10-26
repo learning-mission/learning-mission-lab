f7b665f545122a55a108b40a9ec4bca2dad5d76a
Garush <garush.mkhitaryan@gmail.com>
2021-10-26 20:07:56 +0400

    created Module and Lesson classes and ModuleLevel enum

--- LearningMissionLab/Module.cs
+++ LearningMissionLab/Module.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace LearningMissionLab
 {
-    public class Module
+    public class Module: Unit<Lesson>
     {
-        public Module()
+        string subjectID;
+        ModuleLevel moduleLevel;
+        List<ModuleLevel> ListOfPrerequisites = new List<ModuleLevel>();
+                   
+
+        public Module(string subjectID, ModuleLevel moduleLevel, List<ModuleLevel> listOfPrerequisites)
+            :base (subjectID, moduleLevel,listOfPrerequisites)
         {
+           
+            this.subjectID = subjectID;
+            this.moduleLevel = moduleLevel;
+            ListOfPrerequisites = listOfPrerequisites;
         }
+
+        public string SubjectID { get => subjectID; set => subjectID = value; }
+        public ModuleLevel ModuleLevel { get => moduleLevel; set => moduleLevel = value; }
+        public List<ModuleLevel> ListOfPrerequisites1 { get => ListOfPrerequisites; set => ListOfPrerequisites = value; }
     }
 }
