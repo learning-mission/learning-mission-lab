57a55792632d7b69f915333e39a9a69c3ff73889
Garush <garush.mkhitaryan@gmail.com>
2021-10-29 02:36:01 +0400

    2 update create student

--- LearningMissionLab/Student.cs
+++ LearningMissionLab/Student.cs
@@ -10,16 +10,16 @@ namespace LearningMissionLab
     public class Student : Profile
     {
         string _coverLetter;
-        List<Student> _recommendationList;
-        List<Student> _complatedModuleList;
+        List<String> _recommendationList;
+        List<Module> _completedModuleList;
         List<Student> _classroomList;
         Schedule _schedule;
-        public Student(string coverLetter, List<Student> recommendationList, List<Student> complatedModuleList, List<Student> classroomList, Schedule schedule)
+        public Student(string coverLetter, List<String> recommendationList, List<Module> completedModuleList, List<Student> classroomList, Schedule schedule)
                        : base()
         {
             this._coverLetter = coverLetter;
             this._recommendationList = recommendationList;
-            this._complatedModuleList = complatedModuleList;
+            this._completedModuleList = completedModuleList;
             this._classroomList = classroomList;
             this._schedule = schedule;
         }
@@ -30,9 +30,9 @@ namespace LearningMissionLab
 
 
         public string CoverLetter { get => _coverLetter; set => _coverLetter = value; }
-        public List<Student> RecommendationList { get => _recommendationList; set => _recommendationList = value; }
-        public List<Student> ComplatedModuleList { get => _complatedModuleList; set => _complatedModuleList = value; }
-        public List<Student> ClassroomList { get => _classroomList; set => _classroomList = value; }
+        public List<String> RecommendationList { get => _recommendationList; set => _recommendationList = value; }
+        public List<Module> ComplatedModuleList { get => _completedModuleList; set => _completedModuleList = value; }        
         public Schedule Schedule { get => _schedule; set => _schedule = value; }
+        public List<Student> ClassroomList { get => _classroomList; set => _classroomList = value; }
     }
 }
