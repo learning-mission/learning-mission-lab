4f8f0bedad850420b9c3e45595014614e23a88c1
Vahe Mkrtchyan <martuni3vahe@gmail.com>
2021-10-28 11:08:09 +0400

    created Lesson class, fields, constructors and properties

--- LearningMissionLab/Lesson.cs
+++ LearningMissionLab/Lesson.cs
@@ -10,5 +10,42 @@ namespace LearningMissionLab
     /// </summary>
     public class Lesson
     {
+        int _subjectId;
+        int _moduleId;
+        int _lessonId;
+        int _duration;
+        string _description;
+        List<String> _videosList;
+        List<String> _slidesList;
+        DateTime createDate;
+        DateTime updateDate;
+
+        public Lesson(int subjectId, int moduleId, int lessonId, string description,
+                      int duration, List<String> videosList, List<String> slidesList)
+        {
+            this._subjectId = subjectId;
+            this._moduleId = moduleId;
+            this._lessonId = lessonId;
+            this._duration = duration;
+            this._description = description;
+            this._videosList = videosList;
+            this._slidesList = slidesList;
+        }
+
+        public Lesson(int lessonId,int duration)
+        {
+            this._lessonId = lessonId;
+            this._duration = duration;
+        }
+
+        public int SubjectId { get => _subjectId; set => _subjectId = value; }
+        public int ModuleId { get => _moduleId; set => _moduleId = value; }
+        public int LessonId { get => _lessonId; set => _lessonId = value; }
+        public string Description { get => _description; set => _description = value; }
+        public int Duration { get => _duration; set => _duration = value; }
+        public List<string> VideosList { get => _videosList; set => _videosList = value; }
+        public List<string> SlidesList { get => _slidesList; set => _slidesList = value; }
+        public DateTime CreateDate { get => createDate; set => createDate = value; }
+        public DateTime UpdateDate { get => updateDate; set => updateDate = value; }
     }
 }
