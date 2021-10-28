ee50d8d866baac8aa39b5eb4c3ce9f93f558578d
Mher <lalazaryan91@inbox.ru>
2021-10-28 19:49:04 +0400

    fixed formatting issues

--- LearningMissionLab/Classroom.cs
+++ LearningMissionLab/Classroom.cs
@@ -6,16 +6,16 @@ namespace LearningMissionLab
 {
     /// <summary>
     /// Class: Classroom
-    /// Purpose: Provides a model for Learning Mission user classroom 
+    /// Purpose: Provides a model for Learning Mission  classroom 
     /// </summary>
-    public class Classroom:Unit<Student>
+    public class Classroom : Unit<Student>
     {
         int _maximumCapacity;
         int _minimumCapacity;
         Module _module;
         Schedule _schedule;
         public Classroom(int maximumCapacity, int minimumCapacity, Module module, Schedule schedule, string name,
-                        string description, List<Student> itemList):base(UnitType.Module,name, description, itemList)
+                        string description, List<Student> itemList) : base(UnitType.Classroom, name, description, itemList)
         {
             this._maximumCapacity = maximumCapacity;
             this._minimumCapacity = minimumCapacity;
--- LearningMissionLab/Instructor.cs
+++ LearningMissionLab/Instructor.cs
@@ -5,23 +5,23 @@ namespace LearningMissionLab
 {
     /// <summary>
     /// Class: Instructor
-    /// Purpose: Provides a model for Learning Mission user instructor 
+    /// Purpose: Provides a model for Learning Mission instructor 
     /// </summary>
     public class Instructor:Profile
     {
         List<Module> _moduleList;
         List<Classroom> _classroomList;
-        Schedule _scheule;
+        Schedule _schedule;
         public Instructor(List<Module> moduleList, List<Classroom> classroomsList, Schedule schedule, string firstName,
                           string lastName, ContactInfo contactInfo):base(firstName, lastName, contactInfo)
         {
             this._moduleList = moduleList;
             this._classroomList = classroomsList;
-            this._scheule = schedule;
+            this._schedule = schedule;
         }
 
         public List<Module> ModuleList { get => _moduleList; set => _moduleList = value; }
         public List<Classroom> ClassroomList { get => _classroomList; set => _classroomList = value; }
-        public Schedule Scheule { get => _scheule; set => _scheule = value; }
+        public Schedule Scheule { get => _schedule; set => _schedule = value; }
     }
 }
