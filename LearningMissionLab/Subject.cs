683785d9c9ecbbb8ffc9084a7c20f232044f3c73
Edvard Soghomonyan <e.soghomonyan98@gmail.com>
2021-10-29 21:39:23 +0400

    removed redundant spaces, sorry i did not want to

--- LearningMissionLab/Subject.cs
+++ LearningMissionLab/Subject.cs
@@ -8,14 +8,12 @@ namespace LearningMissionLab
     public class Subject
     {
         SubjectType _subjectType;
+
         public Subject(SubjectType subjectType)
         {
             this._subjectType = subjectType;
         }
+
         public SubjectType SubjectType { get => _subjectType; set => _subjectType = value; }
-
-
     }
-
-
 }
