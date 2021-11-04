60490229d77def73a16d292a6ed66ac0d5ee29e0
Mher <lalazaryan91@inbox.ru>
2021-11-04 18:19:04 +0400

    created SimulationConstants class

---
+++ LearningMissionSimulation/SimulationConstants.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningMissionSimulation
+{
+    public class SimulationConstants
+    {
+        // Minimum valid applicant age
+        public static readonly byte ApplicantMinAge = 18;
+        // Maximum valid applicant age
+        public static readonly byte ApplicantMaxAge = 70;
+        /// <summary>Generates random date of birth values within the specified age range./>
+        /// <param name="minAge">The minimum age.</param>
+        /// <param name="maxAge">The maximum age.</param>
+        /// <returns>The DateTime value within specified range.</returns>
+        public static Random random = new Random();
+
+        public static readonly string[] syllablePool = { "Ab", "Saa", "Levo", "Pari", "Rub", "Ask",
+            "Mam", "Ket", "Zar", "Luci", "Ter", "Ova", "Sar", "Vol", "Ver" };
+
+        public static readonly string[] maleNamePool = { "Sevak", "Mher", "Arevshat",
+            "Garush", "Karen", "Smbat", "Rouben", "Garegin", "Vahe", "Eduard",
+            "Gavril", "Suren", "Arkadij" };
+
+        public static readonly string[] femaleNamePool = { "Nina", "Karine", "Margarita",
+            "Narine", "Nane", "Marina", "Lilit", "Yelena" };
+        static readonly string[] lastNamePool = new string[] { "Lalazryan", "Mkhrtchyan",
+            "Hakobyan", "Vardanyan", "Lobyan", "Levonyan", "Sahakyan", "Gevorgyan" };
+
+        public static readonly string[] alphabetVocalLetterPool = new string[] { "a", "e", "i", "o", "u", "y" };
+
+        public static readonly string[] alphabetConsonantLetterPool = new string[] { "b", "c", "d", "f", "g",
+            "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z" };
+
+
+        public static readonly string countryCode = "+374";
+        public static readonly string[] phoneOperatorCode = { "10", "11", "33", "44", "47", "55", "77", "91", "93", "94", "95", "96", "97", "98", "99" };
+
+        public static readonly char[] letterPool = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                                           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public static readonly List<string> domainPool = new List<string>()
+            {"@email.ru", "@gmail.com", "@yahoo.com", "@yandex.ru"};
+        public  static object keyValueCityCode;
+        public static readonly Dictionary<string, string> CityDictionary = new Dictionary<string, string>()
+            {
+              //Region Ararat
+              { "0613", "Noyakert" },
+              { "0605", "Avshar" },
+              { "0604", "Aygavan" },
+              //Region Aparan 
+              { "0301", "Aparan" },
+              { "0302", "Aragatz" },
+              { "0305", "Hartavan" },
+              //Region Armavir 
+              { "0912", "Amasia" },
+              { "0915", "Araqs" },
+              { "0914", "Arazap" },
+              //Region Gegharquniq 
+              { "1403", "Astghadzor" },
+              { "1412", "Dzoragyugh" },
+              { "1405", "Geghhovit" },
+              //Region Kotayq
+              { "2201 - 2208", "Abovyan" },
+              { "2209", "Akunq" },
+              { "2211", "Aramus" },
+              //Region Lori
+              { "1821", "Jrashen" },
+              { "1812", "Lusaghbyur" },
+              { "1822", "Saralanj" },
+              //Region Syuniq 
+              { "2613", "Haykavan" },
+              { "2614", "Hatsik" },
+              { "2615", "Hovuni" },
+              //Region Shirak
+              { "3010", "Gexanist" },
+              { "3011", "Getap" },
+              { "3014", "Haritsh" },
+              //Region Tavush
+              { "4210", "Norashen" },
+              { "4214", "Paravaqar" },
+              { "4205", "Tovuz" },
+              //Region Vayoc dzor
+              { "3606", "Getap" },
+              { "3607", "Gladzor" },
+              { "3609", "Hermon" },
+              //Erevan
+              { "0001", "Yerevan" }
+            };
+
+    }
+}
