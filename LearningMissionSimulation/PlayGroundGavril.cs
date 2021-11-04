67b021544442a7e021bc748c9f969f2c69a713b8
Gavril Alaverdyan <alaverdyangavril@gmail.com>
2021-11-04 23:48:43 +0400

    created class playgroundgavril

---
+++ LearningMissionSimulation/PlayGroundGavril.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningMissionSimulation
+{
+    public  class PlayGroundGavril
+    {
+        public PlayGroundGavril()
+        {
+
+        }
+    }
+}
