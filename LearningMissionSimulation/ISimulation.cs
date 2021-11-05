8c195e8b22b67cc98b2726d66cb1cf71c2b7be06
Yelena Meroyan <ymeroyan@gmail.com>
2021-11-04 20:11:50 -0700

    added comments and method placeholder to ObjectGenerator and AttributeGenerator; Added interface ISimulation

---
+++ LearningMissionSimulation/ISimulation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LearningMissionSimulation
+{
+    public interface ISimulation
+    {
+        void CreateAccounts();
+
+        void ActivateAccounts();
+
+        void AssignInstructorsToCreateModules();
+
+        void AssignCompletedModulesToStudents();
+
+        void AssignInstructorsForClasses();
+
+        void RegisterStudentsForClasses();
+
+    }
+}
