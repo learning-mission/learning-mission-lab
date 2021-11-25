fea1df1ac7d137251777bd6d28806e33ad80d2f7
Garush <garush.mkhitaryan@gmail.com>
2021-11-25 09:36:24 +0400

    implementied reports

--- LearningMissionSimulation/PlaygroundGarush.cs
+++ LearningMissionSimulation/PlaygroundGarush.cs
@@ -7,6 +7,7 @@ namespace LearningMissionSimulation
 {
     public class PlaygroundGarush : ISimulation
     {
+        ReportType _reportType;
         List<Account> pendingAccountList = new List<Account>();
         List<Student> studentList = new List<Student>();
         List<Instructor> instructorList = new List<Instructor>();
@@ -16,10 +17,18 @@ namespace LearningMissionSimulation
         Dictionary<Guid, Module> moduleDictionary = new Dictionary<Guid, Module>();
         List<Guid> moduleIdList = new List<Guid>();
 
+        public PlaygroundGarush(ReportType reportType)
+        {
+            this.ReportType = reportType;
+        }
+        public ReportType ReportType { get => _reportType; set => _reportType = value; }
+
         public void CreateAccounts(int accountCount)
         {
+            string action = " create account ";
+            ReportHeader(actionName: action);
             for (int i = 0; i < accountCount; i++)
-            {
+            {                
                 Account account = ObjectGenerator.GenerateAccount();
 
                 accountDictionary.Add(account.Id, account);
@@ -31,6 +40,7 @@ namespace LearningMissionSimulation
                     {
                         pendingAccountList.Add(account);
                     }
+                    student.Report();
                 }
                 else if (account.Role == Role.Instructor)
                 {
@@ -40,30 +50,43 @@ namespace LearningMissionSimulation
                     {
                         pendingAccountList.Add(account);
                     }
-                }
-
-                Console.WriteLine("create account {0} \n", account);
+                    instructor.Report();
+                }               
             }
+
+            ReportSummary(actionName: action, count: accountCount);
+            ReportFooter(actionName: action);
         }
 
         public void ActivateAccounts()
         {
+            string action = "activate account";
+            int item = 0;
+            ReportHeader(actionName: action);
             for (int i = 0; i < pendingAccountList.Count; i++)
             {
                 pendingAccountList[i].Status = Status.Active;
                 Console.WriteLine($" activated account is{pendingAccountList[i]}\n");
-            }
-            Console.WriteLine($"count is  {pendingAccountList.Count}");
+            }            
+           
+            ReportSummary(actionName: action, count: item);
+            ReportFooter(actionName: action);
             pendingAccountList.Clear();
         }
 
         public void AssignModulesToInstructors()
         {
+            string action = "Assign modules to instructor";
+            int count = 0;
+            ReportHeader(actionName: action);
             foreach (var instructor in instructorList)
             {
                 instructor.ModuleList = GetModuleList();
                 Console.WriteLine(instructor);
             }
+
+            ReportSummary(actionName: action, count: count);
+            ReportFooter(actionName: action);
         }
 
         List<Module> GetModuleList()
@@ -93,28 +116,42 @@ namespace LearningMissionSimulation
 
         public void AssignModulesToStudents()
         {
+            string action = "Assing Modules to students";
+            int count = 0;
+            ReportHeader(actionName: action);
             foreach (var student in studentList)
             {
                 student.CompletedModuleList = GetModuleList();
                 Console.WriteLine(student);
             }
+
+            ReportSummary(actionName: action, count: count);
+            ReportFooter(actionName: action);
         }
 
         public void CreateSubjects(int subjectCount)
         {
+            string action = "Subject generation";
+            ReportHeader(actionName: action);
             int i = 0;
             while (i < subjectCount)
-            {
+            {               
                 Subject subject = ObjectGenerator.GenerateSubject();
                 subjectList.Add(subject);
                 subjectIdList.Add(subject.Id);
                 Console.WriteLine("\ncreate subject {0}\n", subject);
                 i++;
+                ReportItem(itemName: subject.ToString(), actionName: action, itemIndex: i);
             }
+
+            ReportSummary(actionName: action, count:subjectCount);
+            ReportFooter(actionName: action);
         }
 
         public void CreateModules(int moduleCount)
         {
+            string action = "Subject generation";
+            ReportHeader(actionName: action);
             int i = 0;
             while (i < moduleCount)
             {
@@ -124,7 +161,11 @@ namespace LearningMissionSimulation
                 moduleIdList.Add(module.Id);
                 Console.WriteLine("\ncreate module {0}\n", module);
                 i++;
+                ReportItem(itemName: module.ToString(), actionName: action, itemIndex: i);
             }
+
+            ReportSummary(actionName: action, count: moduleCount);
+            ReportFooter(actionName: action);
         }
 
         public void CreateClassrooms(int classroomCount)
@@ -147,5 +188,59 @@ namespace LearningMissionSimulation
             // Clear all internal data structures 
             throw new NotImplementedException();
         }
+
+        #region Reports
+        void ReportHeader(string actionName)
+        {
+            switch (this.ReportType)
+            {
+                case ReportType.Verbose:
+                case ReportType.Short:
+                case ReportType.Error:
+                    Console.WriteLine($"******{actionName} is started******\n");
+                    break;
+            }
+        }
+
+        void ReportFooter(string actionName)
+        {
+            switch (this.ReportType)
+            {
+                case ReportType.Verbose:
+                case ReportType.Short:
+                case ReportType.Error:
+                    Console.WriteLine($"******{actionName} is finished******\n");
+                    break;
+            }
+        }
+
+        void ReportItem(string itemName, string actionName, int itemIndex)
+        {
+            if (this.ReportType == ReportType.Verbose)
+            {
+                Console.WriteLine($" {actionName} {itemIndex} {itemName}\n");
+            }
+        }
+
+        void ReportSummary(string actionName, int count)
+        {
+            if (this.ReportType == ReportType.Verbose || this.ReportType == ReportType.Short)
+            {
+                Console.WriteLine($"'----{actionName} {count}----\n");
+            }
+        }
+
+        void ReportError(string missingResource, string failedAction)
+        {
+            switch (this.ReportType)
+            {
+                case ReportType.Error:
+                case ReportType.Verbose:
+                case ReportType.Short:
+                    Console.WriteLine("xxx You do not have the appropriate {0} to {1} xxx\n", missingResource, failedAction);
+                    break;
+            }
+        }
+        #endregion Reports
     }
 }
