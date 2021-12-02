7a478791f795c280c5d5da9d5ddfe057e4f79198
Vahe Mkrtchyan <martuni3vahe@gmail.com>
2021-12-02 23:45:29 +0400

    updated FinancialInfo class

--- LearningMissionLab/FinancialInfo.cs
+++ LearningMissionLab/FinancialInfo.cs
@@ -8,27 +8,26 @@ namespace LearningMissionLab
     {
         readonly Guid _financialId;
         FinancialInfo _financialAid;
-        FinancialInfo _salary;
-        FinancialInfo _stipend;
+        Dictionary<Guid, FinancialInfo> _salaryPoolDictionary = new Dictionary<Guid, FinancialInfo>();
+        Dictionary<Guid, FinancialInfo> _stipendPoolDictionary = new Dictionary<Guid, FinancialInfo>();
         DateTime _createDate;
         DateTime _endDate;
 
-        public FinancialInfo(Guid financialId, FinancialInfo financialAid, FinancialInfo salary,
-                             FinancialInfo stipend, DateTime createDate, DateTime endDate)
+        public FinancialInfo(Guid financialId, FinancialInfo financialAid, Dictionary<Guid, FinancialInfo> salaryPoolDictionary,
+                             Dictionary<Guid, FinancialInfo> stipendPoolDictionary, DateTime createDate, DateTime endDate)
         {
             this._financialId = financialId;
             this._financialAid = financialAid;
-            this._salary = salary;
-            this._stipend = stipend;
+            this._salaryPoolDictionary = salaryPoolDictionary;
+            this._stipendPoolDictionary = stipendPoolDictionary;
             this._createDate = createDate;
             this._endDate = endDate;
         }
 
         public Guid FinancialId => _financialId;
-
         public FinancialInfo FinancialAid { get => _financialAid; set => _financialAid = value; }
-        public FinancialInfo Salary { get => _salary; set => _salary = value; }
-        public FinancialInfo Stipend { get => _stipend; set => _stipend = value; }
+        public Dictionary<Guid, FinancialInfo> SalaryPoolDictionary { get => _salaryPoolDictionary; set => _salaryPoolDictionary = value; }
+        public Dictionary<Guid, FinancialInfo> StipendPoolDictionary { get => _stipendPoolDictionary; set => _stipendPoolDictionary = value; }
         public DateTime CreateDate { get => _createDate; set => _createDate = value; }
         public DateTime EndDate { get => _endDate; set => _endDate = value; }
     }
