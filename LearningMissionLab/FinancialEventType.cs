4851915540580938870b922a5e63b236edbe17f4
Vahe Mkrtchyan <martuni3vahe@gmail.com>
2021-12-03 23:02:54 +0400

    updated FinancialInfo class  created financial event type enum

---
+++ LearningMissionLab/FinancialEventType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningMissionLab
+{
+    public enum FinancialEventType
+    {
+        Unspecified, 
+        Stipend, 
+        FinancialAid, 
+        Salary,
+    }
+}
--- LearningMissionLab/FinancialInfo.cs
+++ LearningMissionLab/FinancialInfo.cs
@@ -7,17 +7,18 @@ namespace LearningMissionLab
     public class FinancialInfo
     {
         readonly Guid _financialId;
-        FinancialInfo _financialAid;
+        Dictionary<Guid, FinancialInfo> _financialAidPoolDictionary = new Dictionary<Guid, FinancialInfo>();
         Dictionary<Guid, FinancialInfo> _salaryPoolDictionary = new Dictionary<Guid, FinancialInfo>();
         Dictionary<Guid, FinancialInfo> _stipendPoolDictionary = new Dictionary<Guid, FinancialInfo>();
         DateTime _createDate;
         DateTime _endDate;
 
-        public FinancialInfo(Guid financialId, FinancialInfo financialAid, Dictionary<Guid, FinancialInfo> salaryPoolDictionary,
-                             Dictionary<Guid, FinancialInfo> stipendPoolDictionary, DateTime createDate, DateTime endDate)
+        public FinancialInfo(Guid financialId, Dictionary<Guid, FinancialInfo> financialAidPoolDictionary, 
+                             Dictionary<Guid, FinancialInfo> salaryPoolDictionary,Dictionary<Guid, FinancialInfo> stipendPoolDictionary, 
+                             DateTime createDate, DateTime endDate)
         {
             this._financialId = financialId;
-            this._financialAid = financialAid;
+            this._financialAidPoolDictionary = financialAidPoolDictionary;
             this._salaryPoolDictionary = salaryPoolDictionary;
             this._stipendPoolDictionary = stipendPoolDictionary;
             this._createDate = createDate;
@@ -25,7 +26,7 @@ namespace LearningMissionLab
         }
 
         public Guid FinancialId => _financialId;
-        public FinancialInfo FinancialAid { get => _financialAid; set => _financialAid = value; }
+        public Dictionary<Guid, FinancialInfo> FinancialAidPoolDictionary { get => _financialAidPoolDictionary; set => _financialAidPoolDictionary = value; }
         public Dictionary<Guid, FinancialInfo> SalaryPoolDictionary { get => _salaryPoolDictionary; set => _salaryPoolDictionary = value; }
         public Dictionary<Guid, FinancialInfo> StipendPoolDictionary { get => _stipendPoolDictionary; set => _stipendPoolDictionary = value; }
         public DateTime CreateDate { get => _createDate; set => _createDate = value; }
