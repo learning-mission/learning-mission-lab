46296d8a029f63c14c04e99bb017f02a63ceddf1
Mher <lalazaryan91@inbox.ru>
2021-12-05 23:25:19 +0400

    changed Schedule constructor

--- LearningMissionLab/Schedule.cs
+++ LearningMissionLab/Schedule.cs
@@ -17,11 +17,16 @@ namespace LearningMissionLab
             this._startDate = startDate;
             this._finishDate = finishDate;
             this._scheduleEventList = scheduleEventList;
-            this._createDate = DateTime.UtcNow;
-            this._updateDate = DateTime.UtcNow;
+            this._createDate = createDate;
+            this._updateDate = updateDate;
+        }
+        public Schedule(DateTime startDate, DateTime finishDate, List<ScheduleEvent> scheduleEventList)
+        {
+            this._startDate = startDate;
+            this._finishDate = finishDate;
+            this._scheduleEventList = scheduleEventList;
         }
 
-    
         public DateTime StartDate { get => _startDate; set => _startDate = value; }
         public DateTime FinishDate { get => _finishDate; set => _finishDate = value; }
         public List<ScheduleEvent> ScheduleEventList { get => _scheduleEventList; set => _scheduleEventList = value; }
